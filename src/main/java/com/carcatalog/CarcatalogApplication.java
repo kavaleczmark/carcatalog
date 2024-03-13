ef163373270ccde860ef56589dcb107ffe4a838b
Mark Kavalecz <kiskav12@gmail.com>
2024-03-13 19:49:34 +0100

    Initial commit

---
+++ src/main/java/com/carcatalog/CarcatalogApplication.java
@@ -0,0 +1,13 @@
+package com.carcatalog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarcatalogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarcatalogApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carcatalog/CarcatalogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carcatalog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarcatalogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
